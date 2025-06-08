0dc6d23c2465645d973f157833cc970c568496f9
M-Polonczyk <m.polonczyk@outlook.com>
2025-06-08 21:17:39 +0200

    dupa

--- main.py
+++ main.py
@@ -29,9 +29,6 @@ def main():
     # config = fetch_pfsense_config()
 
     print(f"{config.dhcpd.lan.range=}")
-    config.dhcpd.lan.range.from_ = "192.168.1.10"
-    config.dhcpd.lan.range.to = "192.168.1.120"
-    print(f"{config.dhcpd.lan.range=}")
 
     # push_pfsense_config(config=config)
 
